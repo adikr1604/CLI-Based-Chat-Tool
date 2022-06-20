7eff9f1ce9dee94db7d60af7246c3b52f1a42e5d
ADITYA KUMAR MANDIWAL <adikr1604@gmail.com>
2022-06-20 20:05:29 +0530

    commit 2

--- server.py
+++ server.py
@@ -1,1 +1,10 @@
-print("cghgj")
+import socket, threading
+host = '127.0.0.1'
+port = 9998
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+clients = []
+names = []
